d8db989b020c3a44111070f1df8b12c5531f2915
QueenUuchuni <dwl.mobilerepairs@gmail.com>
2025-03-02 21:46:13 +0000

    updated setupimage sto use anyand all images inside image folder

--- setup_images.py
+++ setup_images.py
@@ -1,11 +1,13 @@
 from local import run_command
+import os
 
-
-image_paths = [
-    "/images/ceos.tar"
-    ]
-
+image_directory = "/images/"
 
 def setup_unavailable_images():
-    for image in image_paths:
-        run_command("kubectl cp /images/ceos.tar kube-system/kube-controller-manager-knius:/tmp", f"Copying {image} into kube-controller")
+    image_files = [f for f in os.listdir(image_directory) if os.path.isfile(os.path.join(image_directory, f))]
+
+    for image in image_files:
+        image_path = os.path.join(image_directory, image)
+        run_command(f"kubectl cp {image_path} kube-system/kube-controller-manager-knius:/tmp", f"Copying {image} into kube-controller")
+
+setup_unavailable_images()
